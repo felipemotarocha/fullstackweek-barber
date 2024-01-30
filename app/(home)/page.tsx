c8df941cd28e7864e4737ab70dd469a08ae4a348
Felipe Rocha <felipemota.rocha@gmail.com>
2024-01-29 21:45:36 -0300

    feat: add initial booking component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,6 +2,7 @@ import { format } from "date-fns";
 import Header from "../_components/header";
 import { ptBR } from "date-fns/locale";
 import Search from "./_components/search";
+import BookingItem from "../_components/booking-item";
 
 export default function Home() {
   return (
@@ -19,6 +20,11 @@ export default function Home() {
 
       <div className="px-5 mt-6">
         <Search />
+      </div>
+
+      <div className="px-5 mt-6">
+        <h2 className="text-xs mb-3 uppercase text-gray-400 font-bold">Agendamentos</h2>
+        <BookingItem />
       </div>
     </div>
   );
---
+++ app/_components/booking-item.tsx
@@ -0,0 +1,34 @@
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Badge } from "./ui/badge";
+import { Card, CardContent } from "./ui/card";
+
+const BookingItem = () => {
+  return (
+    <Card>
+      <CardContent className="p-5 flex justify-between py-0">
+        <div className="flex flex-col gap-2 py-5">
+          <Badge className="bg-[#221C3D] text-primary hover:bg-[#221C3D] w-fit">Confirmado</Badge>
+          <h2 className="font-bold">Corte de Cabelo</h2>
+
+          <div className="flex items-center gap-2">
+            <Avatar className="h-6 w-6">
+              <AvatarImage src="https://utfs.io/f/0ddfbd26-a424-43a0-aaf3-c3f1dc6be6d1-1kgxo7.png" />
+
+              <AvatarFallback>A</AvatarFallback>
+            </Avatar>
+
+            <h3 className="text-sm">Vintage Barber</h3>
+          </div>
+        </div>
+
+        <div className="flex flex-col items-center justify-center px-3 border-l border-solid border-secondary">
+          <p className="text-sm">Fevereiro</p>
+          <p className="text-2xl">06</p>
+          <p className="text-sm">09:45</p>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default BookingItem;
