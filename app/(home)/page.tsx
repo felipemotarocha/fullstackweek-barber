62123938a298eab5a71d8bc6924049710a483aef
Felipe Rocha <felipemota.rocha@gmail.com>
2024-01-29 21:25:42 -0300

    feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,20 @@
-import Image from "next/image";
+import { format } from "date-fns";
 import Header from "../_components/header";
+import { ptBR } from "date-fns/locale";
 
 export default function Home() {
   return (
     <div>
       <Header />
+
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, Miguel!</h2>
+        <p className="capitalize text-sm">
+          {format(new Date(), "EEEE',' dd 'de' MMMM", {
+            locale: ptBR,
+          })}
+        </p>
+      </div>
     </div>
   );
 }
