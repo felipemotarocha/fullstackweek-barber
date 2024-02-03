42ed3c557dca15294050b979871ad25b39ed0574
Felipe Rocha <felipemota.rocha@gmail.com>
2024-02-02 21:01:20 -0300

    style: update barbershop item top padding

--- app/(home)/_components/barbershop-item.tsx
+++ app/(home)/_components/barbershop-item.tsx
@@ -21,7 +21,7 @@ const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
 
   return (
     <Card className="min-w-full max-w-full rounded-2xl">
-      <CardContent className="px-1 py-0">
+      <CardContent className="px-1 py-0 pt-1">
         <div className="w-full h-[159px] relative">
           <div className="absolute top-2 left-2 z-50">
             <Badge variant="secondary" className="opacity-90 flex gap-1 items-center top-3 left-3">
